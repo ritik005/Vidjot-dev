4f1f79fe00b9ac83f4eccd549e20c1e4b386b4e3
ritikpy <ritikvarshney2014@gmail.com>
2020-04-04 10:48:45 +0530

    Third commit

---
+++ models/Idea.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+ 
+// create Schema
+const IdeaSchema = new Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    details:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:Date,
+        default:Date.now
+    }
+});
+
+mongoose.model('ideas', IdeaSchema);
